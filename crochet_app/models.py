116d7f46cd22f324c31dfc8fee01b632bc1a5cd2
louisesykes198 <louisesykes198@yahoo.com>
2025-04-11 10:38:51 +0100

    Added models.py and admi.py for crochet items

--- crochet_app/models.py
+++ crochet_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CrochetItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='crochet_images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
