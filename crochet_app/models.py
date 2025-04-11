9f2ca8ef5d82bde158ce607106a8f06fdee0e86f
louisesykes198 <louisesykes198@yahoo.com>
2025-04-11 11:35:49 +0100

    Updated models

--- crochet_app/models.py
+++ crochet_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 class CrochetItem(models.Model):
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts", default=1
+    )
     description = models.TextField()
-    image = models.ImageField(upload_to='crochet_images/')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    
-    def __str__(self):
-        return self.name
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
