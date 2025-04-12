72a9c1cf8598ea757819db29ae22c4e5381196bb
louisesykes198 <louisesykes198@yahoo.com>
2025-04-12 10:24:28 +0100

    Added summernotes model to the project

--- crochet_app/admin.py
+++ crochet_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import CrochetItem, Comment
+from django_summernote.admin import SummernoteModelAdmin
+from .models import CrochetItem  # Import your model
 
-admin.site.register(CrochetItem)
-admin.site.register(Comment)
+@admin.register(CrochetItem)
+class CrochetAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)  # Specify the fields to use Summernote editor
+
+
--- crochetcrafts/urls.py
+++ crochetcrafts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include  # include is important here
 
 urlpatterns = [
     path('admin/', admin.site.urls),           # Admin panel
+    path('summernote/', include('django_summernote.urls')),
     path('', include('crochet_app.urls')),     # Your app's URLs
 ]
