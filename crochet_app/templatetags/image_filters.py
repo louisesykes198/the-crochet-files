b910fea3fa57daddecd0b79eefc05606d6498a1d
louisesykes198 <louisesykes198@yahoo.com>
2025-04-30 11:37:14 +0100

    Fix webP images

--- crochet_app/templatetags/image_filters.py
+++ crochet_app/templatetags/image_filters.py
@@ -5,9 +5,6 @@ register = template.Library()
 
 @register.filter
 def to_webp(image_url):
-    """
-    Converts an image URL ending in .png or .jpg/.jpeg to .webp.
-    """
     base, ext = os.path.splitext(image_url)
     if ext.lower() in ['.png', '.jpg', '.jpeg']:
         return f"{base}.webp"
